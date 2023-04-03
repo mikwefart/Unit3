5512e842c5f547f1676b43e09dce08df2a5de193
Hernandez <748381@student.dusd.net>
2023-04-03 11:40:48 -0700

    it finally moved

--- Unit3Hernandez/Assets/Scripts/SpawnManager.cs
+++ Unit3Hernandez/Assets/Scripts/SpawnManager.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private Vector3 spawnPos = new Vector3(25, 2, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
-    private PlayerController pl
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -20,8 +21,11 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
-    void SpawnObstacle ()
+    void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
